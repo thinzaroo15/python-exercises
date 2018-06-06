2913e555e4c7714dfca7e3a98ebf0354cf88e54c
thinzaroo15 <thinzarookhadka0088@gmail.com>
2018-06-06 10:49:52 +0530

    Exercise 5

--- ex5.py
+++ ex5.py
@@ -1,19 +1,19 @@
 my_name = 'zed A. shaw'
 my_age = 35 #not a lie
 my_height = 74 #inches
-my_weigth = 180 #lbs
+my_weight = 180 #lbs
 my_eyes = 'Blue'
 my_hair = 'brown'
 my_teeth = 'White'
 
-print "Let's talk about %s ." %my_name
-print "He's %d inches tall ." %my_height
-print "He's %d pounds heavy." %my_weight
-print "Actaully that's not too heavy."
-print "He's got %s eyes and %s hair." %(my_eyes,my_hair)
-print "His teeth are usually %s depending on the coffee." %my_teeth
+print (f"Let's talk about {my_name}")
+print (f"He's {my_height} inches tall")
+print (f"He's {my_weight} pounds heavy")
+print (f"Actaully that's not too heavy.")
+print (f"He's got {my_eyes} eyes and {my_hair} hair") 
+print (f"His teeth are usually {my_teeth} depending on the coffee.") 
 
-print "If I add %d ,%d, and %d I get %d ." %(my_age, my_height, my_weigth,my_age + my_height + my_weight)
+print (f"If I add {my_age} ,{my_height}, and {my_weight} I get {my_age + my_height + my_weight}")
 
 
 
