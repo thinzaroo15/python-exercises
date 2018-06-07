9113559c03a1c39186c2edd9edc010e3569a454c
thinzaroo15 <thinzarookhadka0088@gmail.com>
2018-06-07 20:05:06 +0530

    exrcises 19

---
+++ ex19.py
@@ -0,0 +1,16 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print(f"YOu have {cheese_count}cheeses!")
+    print(f"You have {boxes_of_crackers}boxes of crackers1")
+    print("Man that's enough for a party!")
+    print("Get a blankets\n")
+print("We can just give the function numbers directly")
+cheese_and_crackers(20,30)
+print("OR,we can use variable form our script:")
+amount_of_cheese =10
+amount_of_crackers =50
+cheese_and_crackers(amount_of_cheese,amount_of_crackers)
+print("We can even fo math inside too:")
+cheese_and_crackers(10+20,5+6)
+print("And we can conbine the two , variable and math:")
+cheese_and_crackers(amount_of_cheese + 100 ,amount_of_crackers +100)
+
