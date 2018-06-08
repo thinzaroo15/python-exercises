1f096a99bae60132b5a59e67d38b50cbc0630a40
thinzaroo15 <thinzarookhadka0088@gmail.com>
2018-06-08 08:51:42 +0530

    exercise 24

--- ex24.py
+++ ex24.py
@@ -1,5 +1,5 @@
 print("Let's practice everying .")
-print('You\'d need to know \' about escapes with \\that do')
+print('You\'d need to know \' about escapes with \\that do:')
 print('\n newlines and \t tabs')
 poem="""\t The lovely world wit logic so firmly planted cannot discern \n the needs of love nor comprehend passiion form intuition and requires and explanation \n\t\twhere there is none."""
 print("------------------------")
@@ -13,6 +13,10 @@ def decret_formula(started):
     crates = jars/100
     return jelly_beans,jars,crates
 start_point =10000
-beans,jars,crates = secret_formula(start_point)
-print("with a staring point of:{}".format(start_point)
-
+beans,jars,crates = decret_formula(start_point)
+print("with a staring point of:{}".format(start_point))
+print(f"We'have {beans} beans,{jars}jars,and {crates} crates")
+start_point =start_point /10
+print("We can also do that this way:")
+formula = decret_formula(start_point)
+print("WE'd have {} beans,{}jars,and {}crates".format(*formula))
