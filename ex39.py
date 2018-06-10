aeb59db90dd942b6fb8447d1bc4e21b54df6a2b3
thinzaroo15 <thinzarookhadka0088@gmail.com>
2018-06-10 19:11:31 +0530

    exercisse 39

---
+++ ex39.py
@@ -0,0 +1,34 @@
+states ={'Orengon':'OR',
+        'Florida':'FL',
+        'California':'CA',
+        'New York':'NY',
+        'Michigan':'MI',
+        }
+cities ={
+        'CA':'San Francisco',
+        'MI':'Detroit',
+        'FL':'Jackconville'
+        }
+cities['NY']='New York'
+cities['OR']='Portland'
+print ('-'*10)
+print("NY State has:",cities['NY'])
+print("OR State has :",cities['OR'])
+print('-'*10)
+print("Michigan's abbreviation is:",states['Michigan'])
+print("Florida's abbreviation is:",states['Florida'])
+print('-'*10)
+print("Michigan has :",cities[states['Michigan']])
+print("Florida has:",cities[states['Florida']])
+print('-'*10)
+for state,abbrev in list(states.items()):
+    print(f"{state} is abbreviated {abbrev}")
+print('-'*10)
+for abbrev,city in list (cities.items()):
+    print(f"{abbrev} has the city {city}")
+print('-'*10)
+for state,abbrev in list (states.items()):
+    print(f"{state} state is abbreviated {abbrev}")
+    print(f"and has city {cities[abbrev]}")
+print('-'*10)
+state=states.get('Texas')
